7f364c882ce0ad3c3d67f2a3a64c996a2b878c05
Dan Calavrezo <dan.calavrezo.ext@qorix.ai>
2025-05-15 16:24:20 +0300

    template: integrate docs-as-code (#15)
    * template: integrate docs-as-code
    * docs: fixed formatting
    
    Use the `docs-as-code` tool for building documentation
    fixed formatting issues
    
    Addresses: eclipse-score/score#926
    ---------
    
    Signed-off-by: Dan Calavrezo <dan.calavrezo.ext@qorix.ai>

---
+++ docs/conf.py
@@ -0,0 +1,54 @@
+# *******************************************************************************
+# Copyright (c) 2024 Contributors to the Eclipse Foundation
+#
+# See the NOTICE file(s) distributed with this work for additional
+# information regarding copyright ownership.
+#
+# This program and the accompanying materials are made available under the
+# terms of the Apache License Version 2.0 which is available at
+# https://www.apache.org/licenses/LICENSE-2.0
+#
+# SPDX-License-Identifier: Apache-2.0
+# *******************************************************************************
+
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "Module Template Project"
+author = "S-CORE"
+version = "0.1"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+
+extensions = [
+    "sphinx_design",
+    "sphinx_needs",
+    "sphinxcontrib.plantuml",
+    "score_plantuml",
+    "score_metamodel",
+    "score_draw_uml_funcs",
+    "score_source_code_linker",
+    "score_layout",
+]
+
+exclude_patterns = [
+    # The following entries are not required when building the documentation via 'bazel
+    # build //docs:docs', as that command runs in a sandboxed environment. However, when
+    # building the documentation via 'bazel run //docs:incremental' or esbonio, these
+    # entries are required to prevent the build from failing.
+    "bazel-*",
+    ".venv_docs",
+]
+
+templates_path = ["templates"]
+
+# Enable numref
+numfig = True
